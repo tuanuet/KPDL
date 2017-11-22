5f2d5a432cd4fed91a85ad0956bd0a7e65af8099
tuan.vm <tuan.uet@gmail.com>
2017-11-22 15:49:58 +0700

    basic express

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+const { exec } = require('child_process');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  exec('python ./helloworld.py', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    let json = JSON.parse(stdout);
+    json = json.map(item => item[0]*2)
+    res.json(json)
+    // res.render('index', { title: 'Express' });
+  });
+
+});
+
+module.exports = router;
