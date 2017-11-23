5844b84b87a05f5777003f4490237705ff6f0817
tuanuet <tuan.uet@gmail.com>
2017-11-23 08:47:19 +0700

    fix front-end

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,22 @@ const { exec } = require('child_process');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  exec('python ./helloworld.py', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    let json = JSON.parse(stdout);
-    json = json.map(item => item[0]*2)
-    res.json(json)
-    // res.render('index', { title: 'Express' });
-  });
+    exec('python ./helloworld.py', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        let json = JSON.parse(stdout);
+        json = json.map(item => item[0]*2);
+        // res.json(json)
+        res.render('index', { title: 'Hệ thống phân loại bài báo' });
+    });
+});
 
+router.post('/analyze',(req,res) => {
+    console.log(req.body);
+    res.redirect('/')
 });
 
 module.exports = router;
